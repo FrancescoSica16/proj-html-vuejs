0818eaf7829806de8cc02741d428a5b4d33b7c7c
FrancescoSica16 <francesco.sica16@gmail.com>
2021-10-06 15:14:59 +0200

    correzioni in struttura component + reset margini

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: [],
+  
+  data: function() {
+    return{
+      
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../style/general.scss";
+@import "../style/variabiles.scss";
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: [],
+
+  data: function() {
+    return{
+      
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../style/general.scss";
+@import "../style/variabiles.scss";
+</style>
