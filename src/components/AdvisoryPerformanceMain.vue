7aed70e10531d23ab9f9624575cd36613da05862
FrancescoSica16 <francesco.sica16@gmail.com>
2021-10-17 21:20:43 +0200

    advisory section

--- src/components/AdvisoryPerformanceMain.vue
+++ src/components/AdvisoryPerformanceMain.vue
@@ -3,9 +3,18 @@
       
       <div class="container">
           <div class="row">
-              <div class="col-9 m-auto">
-                  
+              <div  class="col-9 m-auto ">
+                <div class="pre-titol  text-center">what are we doing</div>
+                <h1 class="text-center titol-dark"> Advisory<strong>Performance</strong></h1> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, dolores iusto perferendis autem excepturi rerum, nesciunt.</p>               
               </div>
+              <div class="row">
+                <div class="col-9 m-auto">
+                  <img src="../assets/img/video-1.jpg" alt="">
+                </div>
+                
+              </div>
+              
           </div>
       </div>
   </section>
@@ -30,6 +39,22 @@ export default {
 
 #performance{
     background-color: white;
-    height: 400px;
+    
+    margin: 50px 0;
+
+    img{
+      width: 100%;
+    }
+}
+.titol-dark{
+  color: black;
+
+  strong{
+    color:#048383;
+  }
+
+  p{
+    color: $paragGray;
+  }
 }
 </style>
